49768b45133d35401bc56f2d1c5e4967d75650b3
Hannah Dunteman <hannah.dunteman@formetocoupon.com>
2025-12-28 21:52:15 -0600

    initial portfolio setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind Is Working
+      </h1>
+    </main>
+  );
+}
+
+export default App;
+
